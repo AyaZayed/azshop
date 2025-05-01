1cbbf29d43d3cfeb26f401fbc66e4f91ddf68571
Aya Zayed <ayazayedofficial@gmail.com>
2025-05-01 04:30:23 +0300

    metadata error solved

---
+++ app/components/dashboard/ProductFormWrapper.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createProduct } from "@/app/actions/productActions";
+
+import { useFormState } from "react-dom";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { productSchema } from "@/app/lib/zodSchemas";
+import { useState } from "react";
+import ProductForm from "@/app/components/dashboard/ProductForm";
+
+export default function ProductFormWrapper() {
+  const [images, setImages] = useState<string[]>([]);
+  const [lastResult, action] = useFormState(createProduct, undefined);
+
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: productSchema,
+      });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
+  return (
+    <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
+      <ProductForm
+        images={images}
+        setImages={setImages}
+        fields={fields}
+        header="New Product"
+      />
+    </form>
+  );
+}
